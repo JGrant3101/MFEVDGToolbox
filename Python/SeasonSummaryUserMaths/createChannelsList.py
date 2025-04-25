06e6cf365a50766f92c23aa6782385beefdf14d6
JGrant3101 <joe.grant@mclaren-fe.com>
2025-04-25 13:33:12 +0100

    Outputting .json file

--- Python/SeasonSummaryUserMaths/createChannelsList.py
+++ Python/SeasonSummaryUserMaths/createChannelsList.py
@@ -1,60 +1,60 @@
 # Function to produce the list pf dictionaries for the sectors and loops.
 
-def createChannelsList(n):
-    # Start by initialising the list that will be populated with the dictionaries.
-    channels = [None] * n
+def createChannelsList(n, names):
+    # Calculate the number of gating boolean channels that will need to be defined.
+    nBooleans = n + 11
+    # Initialise the list that will be populated with the dictionaries.
+    channels = [None] * nBooleans
 
-    # Loop over the known number of sectors and loops.
-    for i in range (0, 11):
-        # Initialise an empty dictionary.
-        tempDict = {}
+    for i in range (0, nBooleans):
+        # Deal with the 11 known loops first.
+        if i < 11:
+            # Initialise an empty dictionary.
+            tempDict = {}
 
-        # Define the name based on what the index is.
-        if i < 3:
-            # Defining the sector names.
-            tempDict['name'] = 'bI' + str(i + 1)
-        else:
-            # Defining the loop names.
-            tempDict['name'] = 'bL' + str(i - 2)
+            # Assign the name of the boolean zone from the names input array.
+            tempDict['name'] = names[i]
 
-        # Define the units as empty as these are all unitless booleans.
-        tempDict['units'] = ''
+            # Define the units as empty as these are all unitless booleans.
+            tempDict['units'] = ''
 
-        # Define the logical expression based on what the index is.
-        if i == 0 or i == 3:
-            tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap)'
-        elif i == 2 or i == 10:
-            tempDict['expression'] = 'step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
-        else:
-            tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap) * step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
+            # Define the logical expression based on what the index is.
+            if i == 0 or i == 3:
+                tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap)'
+            elif i == 2 or i == 10:
+                tempDict['expression'] = 'step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
+            else:
+                tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap) * step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
 
-        # Define the description as empty as these channels don't need a description.
-        tempDict['description'] = ''
+            # Define the description as empty as these channels don't need a description.
+            tempDict['description'] = ''
 
-        # Finally assign the temporary dictionary to the correct item in the channels list.
-        channels[i] = tempDict
+            # Finally assign the temporary dictionary to the correct item in the channels list.
+            channels[i] = tempDict
+        # Deal with the turn zones, the number of which can vary.
+        else: 
+            # Initialise an empty dictionary.
+            tempDict = {}
 
-    # Loop over the zones that are left.
-    for i in range(11, n):
-        # Initialise an empty dictionary.
-        tempDict = {}
+            # Define the name based on the index.
+            tempDict['name'] = names[i]
 
-        # Define the name based on the index.
-        tempDict['name'] = 'bZ' + str(i - 11)
+            # Define the units as empty as these are all unitless booleans.
+            tempDict['units'] = ''
 
-        # Define the units as empty as these are all unitless booleans.
-        tempDict['units'] = ''
+            # Define the logical expression based on what the index is.
+            if i == 11:
+                tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap)'
+            elif i == nBooleans - 1:
+                tempDict['expression'] = 'step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
+            else:
+                tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap) * step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
 
-        # Define the logical expression based on what the index is.
-        if i == 11:
-            tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap)'
-        elif i == n - 1:
-            tempDict['expression'] = 'step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
-        else:
-            tempDict['expression'] = 'step(track.userPOIs.points[' + str(i) + '].sLapValue - sLap) * step(sLap - track.userPOIs.points[' + str(i - 1) + '].sLapValue)'
+            # Define the description as empty as these channels don't need a description.
+            tempDict['description'] = ''
 
-        # Finall assign the remporary dictionary to the correct item in the channels list.
-        channels[i] = tempDict
+            # Finall assign the remporary dictionary to the correct item in the channels list.
+            channels[i] = tempDict
 
     # Return the now populate list.
     return channels
--- Python/SeasonSummaryUserMaths/createSeasonSummaryUserMaths.py
+++ Python/SeasonSummaryUserMaths/createSeasonSummaryUserMaths.py
@@ -6,15 +6,48 @@
 import numpy as np
 import json
 
-# Define the filepath of an example track .json file to start with.
-trackJSONFilepath = 'C:\\Users\\joe.grant\\Repos\\MFEVDGToolbox\\Python\\SeasonSummaryUserMaths\\Tracks\\S11R08_TKO_POIs.json'
+# Then import any required functions
+from createChannelsList import createChannelsList
 
-# Read in the JSON file.
-with open(trackJSONFilepath, 'r') as trackJSON:
-    trackData = json.load(trackJSON)
+# Let's just define a dummy required number of zones for now.
+numberOfZones = 5
 
-# Extract the track config from the trackData dictionary.
-trackConfig = trackData['config']
+# Start by initialising the userMaths dictionary.
+userMathsDict = {}
 
-# Extract the track POIs from the trackConfig dictionary.
-trackPOIs = trackConfig['userPOIs']['points']
+# Assign the sim version.
+userMathsDict['simVersion'] = '1.12193'
+
+# Assign an empty custom properties dictionary to userMathsDict.
+userMathsDict['customProperties'] = {}
+
+# Create the config dictionary that will be put into userMathsDict later.
+configDict = {}
+
+# Define the list of names for the vector boolean channels that will determine when the car is in various zones and loops around the track
+booleanNames = [None] * (numberOfZones + 11)
+
+for i in range(0, numberOfZones + 11):
+    # Define the name based on what the index is.
+        if i < 3:
+            # Defining the sector names.
+            booleanNames[i] = 'bI' + str(i + 1)
+        elif i > 2 and i < 11:
+            # Defining the loop names.
+            booleanNames[i] = 'bL' + str(i - 3)
+        else:
+            booleanNames[i] = 'bZ' + str(i - 11)
+
+# Creating the channels dictionary for the vector part of config using the sub-function that's been defined.
+channelsDict = createChannelsList(5, booleanNames)
+
+# Assigning this channels dictionary to the config dictionary.
+configDict['vectors'] = {'channels' : channelsDict}
+
+# Assigning the config dictionary to the config key in UserMathsDict.
+userMathsDict['config'] = configDict
+
+# Writing the user maths dictionary to a json file.
+with open('C:\\Users\\joe.grant\\Repos\\MFEVDGToolbox\\Python\\SeasonSummaryUserMaths\\UserMaths\\CreatedUserMaths.json', 'w') as userMathsJSON:
+    json.dump(userMathsDict, userMathsJSON)
+    
